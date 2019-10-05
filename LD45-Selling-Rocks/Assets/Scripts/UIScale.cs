d2ff069e97cc7288301bd76d838173e618dcd1b7
gerh667 <gerh667@gmail.com>
2019-10-05 20:51:46 +0300

    UIScale prefab

---
+++ LD45-Selling-Rocks/Assets/Scripts/UIScale.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScale : MonoBehaviour
+{
+    [SerializeField]
+    private Color m_activeColor;
+    [SerializeField]
+    private Color m_inactiveColor;
+
+    private GameObject[] m_blocks = new GameObject[20];
+
+    public void SetBlockValue(int settingValue)
+    {
+        if (settingValue >= 20)
+        {
+            print("setting value is more than 20");
+            return;
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            if (i < settingValue)
+            {
+                m_blocks[i].GetComponent<Image>().color = m_activeColor;
+                m_blocks[i].GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+            }
+            else if (i == settingValue)
+            {
+                m_blocks[i].GetComponent<Image>().color = m_activeColor;
+                m_blocks[i].GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1f, 1f);
+            }
+            else
+            {
+                m_blocks[i].GetComponent<Image>().color = m_inactiveColor;
+                m_blocks[i].GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+
+
+    }
+}
