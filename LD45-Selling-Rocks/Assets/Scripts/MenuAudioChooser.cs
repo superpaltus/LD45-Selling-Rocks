89e00c8bccc8fd4a280ba499522077ad3817525d
gerh667 <gerh667@gmail.com>
2019-10-07 22:14:45 +0300

    Add planet material changer and main menu audio changer

---
+++ LD45-Selling-Rocks/Assets/Scripts/MenuAudioChooser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAudioChooser : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;
+
+    private void Start()
+    {
+        if (audioClips.Length > 0)
+        {
+            int index = Random.Range(0, audioClips.Length);
+            var audioSource = GetComponent<AudioSource>();
+            audioSource.clip = audioClips[index];
+            audioSource.Play();
+        }
+    }
+}
---
+++ LD45-Selling-Rocks/Assets/Scripts/PlanetMaterialChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMaterialChanger : MonoBehaviour
+{
+    [SerializeField] private Material[] materials;
+
+    private void Start()
+    {
+        if (materials.Length > 0)
+        {
+            int index = Random.Range(0, materials.Length);
+            GetComponent<Renderer>().material = materials[index];
+        }
+    }
+}
